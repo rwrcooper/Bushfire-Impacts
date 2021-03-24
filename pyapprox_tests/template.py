5b46579ae8ed05254891cb24d9e648ed2baef1bb
rwrcooper <rwr.cooper@gmail.com>
2021-03-24 13:51:18 +1100

    add template file

---
+++ pyapprox_tests/template.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+"""
+File: template.py
+Author: riley cooper
+Email: rwr.cooper@gmail.com
+Description:
+    Template script.
+
+Usage:
+    template.py -a <arg1> -b <arg2>... [-c <arg3>]
+    template.py option1 -a <arg1> -b <arg2>... [-c <arg3>]
+                    
+
+Options:
+    -a <arg1>, --arg1=<arg1>
+    -b <arg2>, --arg2s=<arg2>
+    -c <arg3>, --arg3=<arg3>
+
+    -h, --help
+    --option=<n>
+"""
+
+
+# imports
+from docopt import docopt
+
+
+def execute_option1():
+    # run some task
+    for i in range(2):
+    print(i)
+
+
+def main(args):
+
+    if args['option1']:
+    # for example
+        execute_option1()
+
+    arg1 = args['--arg1']
+    # other args...
+
+    return
+
+
+if __name__ == '__main__':
+    args = docopt(__doc__)
+
+    main(args)
+
